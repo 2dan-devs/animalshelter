bb0884e412a4e0111bc740492d0408a1c09eda15
dnavas <dani.gnavas@gmail.com>
2014-12-16 22:07:11 -0500

    Initial Commit

---
+++ app/views/index.php
@@ -0,0 +1,8 @@
+<!doctype html><html class="no-js" ng-app="fe"><head><meta charset="utf-8"><title>fe</title><meta name="description" content=""><meta name="viewport" content="width=device-width"><link rel="stylesheet" href="styles/vendor-ccf6e3f2.css"><link rel="stylesheet" href="styles/app-96fa39a5.css"><script src="scripts/modernizr-c1ff1650.js"></script></head><body><div ui-view=""></div><script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='//www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script><script src="scripts/vendor-6b6ddd0d.js"></script><script src="scripts/app-6288cb7c.js"></script></body></html>
